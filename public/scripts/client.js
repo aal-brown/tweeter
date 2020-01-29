12be446e669f2cfd6bf86db743d6d86883f9f17c
aal-brown <adlai.brown@gmail.com>
2020-01-29 03:04:25 +0000

    Most functionality from day 2 of tweeter app development successfully implemented.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,75 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(() => {
+  console.log("I\'m ready");
+
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+
+  const createTweetElement = function(tweetObj) {
+
+    let tweetTemplate =  `
+    <article class="tweets">
+    <header>
+      <div class="img-name">
+        <img src="${tweetObj.user.avatars}">
+        <span class="name tweets">${tweetObj.user.name}</span>
+      </div>
+      <span class="username tweets">${tweetObj.user.handle}</span>
+    </header>
+      <p><b>${tweetObj.content.text}</b></p>
+    <footer>
+      <span class="time tweets">${tweetObj.created_at}</span>
+      <span class="tweet tweets">W X Y Z</span>
+    </footer>
+    </article>
+  `;
+
+    return tweetTemplate;
+
+  };
+
+
+
+
+/* let $tweet = createTweetElement(tweetData);
+
+$("#tweets-container").append($tweet);
+
+console.log($tweet); */
+
+
+  const renderTweets = function(tweetsObjArr) {
+
+    tweetsObjArr.forEach((value) => {
+      $("#tweets-container").append(createTweetElement(value));
+    });
+  };
+
+  renderTweets(data);
+
+});
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+
+$(document).ready(() => {
+  console.log("I\'m ready");
+});
+
+$("[name='text']").on("keyup",function() {
+  let charRem = 140 - $(this).val().length;
+  
+
+  if (charRem >= 0) {
+    $(".counter").css("color","#545149").text(charRem);
+    
+  } else {
+    $(".counter").css("color","red").text(charRem);
+  }
+
+});
